c171eb22c9c246bee6fc53e63aab6a1b15572d76
illbot <posoroboti123@gmail.com>
2022-06-05 20:42:10 +0200

    Adding Dockerfile

--- kepbongeszo/src/environments/environment.prod.ts
+++ kepbongeszo/src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+
+  API_PATH: "/api/",
+
+  USER: "/api/user",
+  AUTH_SIGNIN: "/api/auth/signin",
+  AUTH_SIGNUP: "/api/auth/signup",
+
+  PIC_UPLOAD: "/api/picture/upload",
+  PIC_UPLOAD_DATA: "/api/picture/uploadData",
+
+  GET_FILE: "/api/picture/files",
+  GET_ALL_PICTURE: "/api/picture/getAll",
+  CHANGE_VISIBILITY: "/api/picture/changeVisibility",
+  DELETE_PICTURE: "/api/picture/delete/"
 };
